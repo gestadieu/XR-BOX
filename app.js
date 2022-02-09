2098f222d546b44fa2bee18660a657085d55dff8
gerald <gestadieu@usj.edu.mo>
2022-02-09 16:22:29 +0800

    change to mindAR.js, generate QRCode for the URL

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT | 7777
+const express = require('express'),
+  app = express(),
+  cors = require('cors'),
+  QRCode = require('qrcode'),
+  address = require('address')
+
+const port = process.env.PORT | 3000
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const cors = require('cors')
 
 app.use(cors())
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({
   extended: true
 }))
@@ -16,10 +19,13 @@ app.use(express.json({
 }));
 
 
-// const mediaRoutes = require('./routes/media')
-// app.use('/api/media', mediaRoutes)
-
-
 http.listen(port, () => {
-  console.log(`listening on port http://localhost:${port}`);
+  const url = `http://${address.ip()}:${port}/index.html`
+  QRCode.toFile('public/assets/urlqrcode.png', url)
+  console.log(`listening on port http://localhost:${port}`)
 });
+
+// Socket.IO:
+// listen input "buttons"
+// -> change image to display fullscreen
+// -> how to change url in the browser???
